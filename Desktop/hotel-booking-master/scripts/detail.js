6b70026463479ec6b4f34e8350fcc682b8654235
saichandra <thammishettisaichandra@gmail.com>
2021-06-11 20:30:39 +0530

    'first'

--- Desktop/hotel-booking-master/scripts/detail.js
+++ Desktop/hotel-booking-master/scripts/detail.js
@@ -1,23 +1,29 @@
 var toDate;
-var fromDate
+var fromDate;
 
- function GetDays(){
-    var adultNumber =document.getElementById("adults").value;
-    var toDate= new Date(document.getElementById("toDate").value);
+function GetDays(){
+    var adultNumber = document.getElementById("adults").value;
+    var toDate = new Date(document.getElementById("toDate").value);
     var fromDate = new Date(document.getElementById("fromDate").value);
     return parseInt((toDate - fromDate) / (24 * 3600 * 1000))*adultNumber*1000;
-
- }
- function call(){
-    var adultNumber =document.getElementById("adults").value;
-    var toDate= new Date(document.getElementById("toDate").value);
+}
+function call(){
+    var adultNumber = document.getElementById("adults").value;
+    var toDate = new Date(document.getElementById("toDate").value);
     var fromDate = new Date(document.getElementById("fromDate").value);
-    if(toDate>fromDate && adultNumber >0  ){
+    if(toDate > fromDate && adultNumber > 0){
         document.getElementById("price").value="Rs."+ GetDays();
-        document.getElementById("bookNow").removeAttribute("disabled")
-
-    }  else{
+        document.getElementById("bookNow").removeAttribute("disabled");
+    }
+    else{
         document.getElementById("price").value="Rs.0";
         document.getElementById("bookNow").setAttribute("disabled",true);
     }
-    }
+}
+function GetDays(){
+    var adultNumber = document.getElementById();
+}
+function call(){
+    var adultNumber = document.getElementById("adults").value;
+    var toDate = new 
+}
